f3c0f1ad61f03e398f0421526531ee47254ae4e6
hafsah-R <76250695+hafsah-R@users.noreply.github.com>
2021-10-25 09:46:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const color=['#FFE4E1','#FFF5EE','#F5F5F5','#FAF0E6','#FFFFE0','#E6E6FA','#F0F8FF','pink'];
-var url="http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
-
+var url="https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
 var rand =Math.floor(Math.random()*color.length); 
 
 var qouteText=""
@@ -14,12 +13,13 @@ function changeColor(){
 var getQuote= function(data){
     
     var quot="https://twitter.com/intent/tweet?text=" + data.quoteText+data.quoteAuthor;
- 
+   qouteText=data.quoteText
+   qouteAuthor=data.quoteAuthor
        
-    $('#text').text(data.quoteText);
+    $('#text').text(qouteText);
 	$('#text').slideUp();
 	$('#text').slideDown();
-    $('#author').text(data.quoteAuthor);
+    $('#author').text(qouteAuthor);
 
 	
 };
